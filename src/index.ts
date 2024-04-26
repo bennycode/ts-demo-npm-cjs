05d808d62dd06f6d04d8467557d60b0d3c296294
Benny Neugebauer <mail@bennycode.com>
2024-04-26 16:54:11 +0200

    test more complicated path

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { removeSuffix } from "helpers/removeSuffix";
+import { removeSuffix } from "@helpers/removeSuffix";
 
 export function remove(str: string, suffix: string) {
   return removeSuffix(str, suffix);
